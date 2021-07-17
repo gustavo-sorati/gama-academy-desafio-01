1a05440a33f52b5ebe883ddf5af501587cc2a479
Gustavo Sorati <gustavo.sorati@outlook.com>
2021-07-16 22:29:16 -0300

    Confete efeito

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { Card } from '../../components/Card';
-import { Confete } from '../../components/Confete';
 
 import styles from './styles.module.scss';
 
